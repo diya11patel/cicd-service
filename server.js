b8b61f7daff47300697109817a14d95cd7945402
Diya Patel <diyapatel4517@gmail.com>
2024-11-18 21:14:09 +0530

    tezst

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-
-console.log("Success")
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("<h1> Deployed Version is v1.0.</h1>");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
